162ef6de328e6b1cc98e8461c59cd922bb49098e
professorastelleana <145031308+professorastelleana@users.noreply.github.com>
2024-11-14 10:14:39 -0300

    correção

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'HALLOWEEN',
+    'Quem é a mãe do Jason Voorhees ?',
+    'Pamela Voorhees'
+)
+
+criaCartao(
+    'HALLOWEEN',
+    'Qual a principal arma de Michael Myers ?',
+    'Faca de cozinha'
+)
+
+criaCartao(
+    'HALLOWEEN',
+    'Qual o nome do vilão do filme A hora do Pesadelo ?',
+    'Freddy Krueger'
+)
+
